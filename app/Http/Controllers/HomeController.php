be4fbfe702a94bf3c2e80e27e24c7c657757986d
Mushfiqul21 <mushfiqulislam21@gmail.com>
2024-10-07 23:18:41 +0600

    Home Dynamic done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
+use App\Models\Job;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('frontend.home');
+        $data['categories'] = Category::where('status',1)
+                                ->orderBy('name','asc')
+                                ->take(8)
+                                ->get();
+
+        $data['featured_jobs'] = Job::where('status',1)
+                                ->orderBy('created_at','desc')
+                                ->with('jobType')
+                                ->where('isFeatured',1)
+                                ->take(6)
+                                ->get();
+
+        $data['latest_jobs'] = Job::where('status',1)
+                                ->orderBy('created_at','desc')
+                                ->take(6)
+                                ->get();
+
+        return view('frontend.home',$data);
     }
 }
